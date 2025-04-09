7ded95d30c9f44fad7093d59ba1851af403d0886
rashmiiyengar <rashmiiyengar255@gmail.com>
2025-04-09 13:45:57 -0700

    Initial commit

---
+++ load-test.js
@@ -0,0 +1,30 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+//staging
+//designed to test /actual values may differ as per application
+export const options={
+    stages: [
+        {
+        duration:'10s',
+        target: 10   
+        },
+        {
+        duration:'30s',
+        target: 10   
+        },
+        {
+        duration:'10s',
+        target: 0    
+        }
+    ]
+}
+
+export default function(){
+    http.get('https://test.k6.io');
+    sleep(1);
+    http.get('https://test.k6.io/contact.php');
+    sleep(2)
+    http.get('https://test.k6.io/news.php');
+    sleep(2);
+}
