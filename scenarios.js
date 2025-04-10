dba0b88c3db76828af746e81d2f6ceee49816bf2
rashmiiyengar <rashmiiyengar255@gmail.com>
2025-04-10 12:09:09 -0700

    added new scenariors for asssertions

--- scenarios.js
+++ scenarios.js
@@ -5,11 +5,11 @@ export default function() {
     const response=http.get('https://test.k6.io')
     
     check(response,{
-        'status code is 200':(r)=> r.status === 200
+        'status code is 200':(r)=> r.status === 200,
+        'Page is home page':(r)=> r.body.includes('Collection of simple web-pages suitable for load testing')
     });
+    
+    
 
-    check(response,{
-        'Page is home page':(r)=> r.body.includes('Collection of simple web-pages suitable for load testing')===true
-    });
     
 }
